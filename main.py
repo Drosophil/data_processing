980197922b7583c4b1882c2c91b9d6afaec6a6b3
Drosophil <dmitriy.filyukov@gmail.com>
2024-04-09 23:03:43 +0300

    Added hyperthreading control parameter in main.py.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from mpp import MolecularPropertiesProcessor
+from mpp import MolecularPropertiesProcessor  #, MolecularPropertiesProcessorIO
 
 if __name__ == '__main__':
     mpp_instance = MolecularPropertiesProcessor(
@@ -6,5 +6,6 @@ if __name__ == '__main__':
         output_file_name="output.csv",
         smiles_column="clean_smiles",
         molecule_id_column="inchikey",
+        hyperthreading=True  # we need real physical cores for the processes
     )
     final_result = mpp_instance.process_data()
